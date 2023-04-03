35f3b274e3a8de419422c6dac55ba49b55d40a3c
DineshKumar22122001 <126236053+DineshKumar22122001@users.noreply.github.com>
2023-04-03 13:23:31 +0530

    modal fix

--- src/components/Slidebar.js
+++ src/components/Slidebar.js
@@ -202,14 +202,6 @@ function Slidebar(props) {
           {drawer}
         </Drawer>
       </Box>
-      <Box
-        component="main"
-        sx={{
-          flexGrow: 1,
-          p: 3,
-          width: { sm: `calc(100% - ${drawerWidth}px)` },
-        }}
-      ></Box>
     </Box>
   );
 }
